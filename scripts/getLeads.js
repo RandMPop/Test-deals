7dddf65c6dfc2d3b4ae4b00c00f5ef6ce6e60426
RandMPop <OlgaCherniaeva99@yandex.ru>
2025-03-21 10:56:38 +0500

    Final version of the code

--- scripts/getLeads.js
+++ scripts/getLeads.js
@@ -1,5 +1,5 @@
 //Запрос на сделки
-const limit =3;
+const limit =3; //Лимит на загрузку сделок
 const apiUrl = 'https://olgachfrontend.amocrm.ru/api/v4/leads?limit=' + limit;
 const accessToken = localStorage.getItem('access_token').toString();
 let leadsIDArray = [];
@@ -10,7 +10,6 @@ let leadIndex = 0;
 
 
 function fillElements (i,data){
-    console.log("leadIndex",leadIndex);
     leadNameAll[i].textContent += data.name;
     leadBudgetAll[i].textContent += data.price;
     leadIDAll[i].textContent += data.id;
@@ -35,7 +34,7 @@ async function fetchPage (url) { //Запрос данных по заданно
     return data;
 }
 
-async function fetchAllPages() {
+async function fetchAllPages() { //Запрос по 1 странице данных, т.е. по 3 сделки
     let currentPage = await fetchPage(apiUrl);
     await sleep(1000);
     currentPage._embedded.leads.forEach(lead => {
@@ -52,95 +51,4 @@ async function fetchAllPages() {
     }
     localStorage.setItem('leadsIDArray',leadsIDArray);   
 }
-
-
-
 fetchAllPages();
-
-
-
-
-
-//Запрос на сделки
-// const apiUrl = 'https://olgachfrontend.amocrm.ru/api/v4/leads';
-// const accessToken = localStorage.getItem('access_token').toString();
-// let leadsIDArray = new Array(10);
-
-// fetch(apiUrl, {
-//     method: 'GET',
-//     headers: {
-//         'Authorization': `Bearer ${accessToken}`,
-//         'Content-Type': 'application/json',
-//     }
-// })
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Ошибка запроса: ' + response.statusText);
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log('Ответ от API:', data);
-//         const leadNameAll = document.querySelectorAll(".lead-data-name");
-//         const leadBudgetAll = document.querySelectorAll(".lead-data-budget");
-//         const leadIDAll = document.querySelectorAll(".lead-data-id");
-//         for (let i = 0; i < 10; i++){
-//             leadsIDArray[i] = data._embedded.leads[i].id;
-//             leadNameAll[i].textContent += data._embedded.leads[i].name;
-//             leadBudgetAll[i].textContent += data._embedded.leads[i].price;
-//             leadIDAll[i].textContent += data._embedded.leads[i].id;
-//         }
-//         localStorage.setItem('leadsIDArray',leadsIDArray);
-//     })
-//     .catch(error => {
-//         console.error('Ошибка:', error);
-//     }); 
-
-
-
-// const apiUrl = 'https://olgachfrontend.amocrm.ru/api/v4/leads?limit=3';
-// const accessToken = localStorage.getItem('access_token').toString();
-// let leadsIDArray = new Array(10);
-
-// function sleep(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
-// async function fetchPage(url) {
-//     const response = await fetch(url, {
-//             method: 'GET',
-//             headers: {
-//                 'Authorization': `Bearer ${accessToken}`,
-//                 'Content-Type': 'application/json',
-//             }
-//     })
-//     if (!response.ok) {
-//         throw new Error(`Ошибка HTTP: ${response.status}`);
-//     }
-//     const data = await response.json();
-//     return data
-    
-// }
-
-// async function fetchArray() {
-//     response = await fetchPage(apiUrl);
-//     await sleep(1000);
-//     let returnArray = [];
-//     response._embedded.leads.forEach(lead => {
-//         returnArray.push(lead.id);
-//     });
-//     while (response._links.next) {
-//         response = await fetchPage(response._links.next.href);
-//         await sleep(1000);
-//         response._embedded.leads.forEach(lead => {
-//             returnArray.push(lead.id);
-//         });
-//     }
-//     return returnArray;
-    
-// }
-
-// fetchArray().then(result => {
-//     leadsIDArray = result;
-//     console.log(leadsIDArray);
-// });
--- scripts/leadsCardsLoading.js
+++ scripts/leadsCardsLoading.js
@@ -1,10 +1,7 @@
 const leadDataAll = document.querySelectorAll(".lead-data");
 const leadDetailsAll = document.querySelectorAll(".lead-details");
-// const apiUrlLead = 'https://cors-anywhere.herokuapp.com/https://olgachfrontend.amocrm.ru/api/v4/leads/3783807'
-// import {leadsIDArray} from './getLeads.js';
 const apiUrlArray = new Array(10);
 leadsUrlID = localStorage.getItem('leadsIDArray',leadsIDArray).split(',');
-// console.log(leadDataAll);
 for (let i = 0; i < 10; i++){
     apiUrlArray[i] = 'https://olgachfrontend.amocrm.ru/api/v4/leads/'+leadsUrlID[i];
 }
@@ -14,19 +11,23 @@ function convertUnixTimestamp(timestamp) { //Преобразование Unix T
     const day = String(date.getDate()).padStart(2, '0'); // Получаем день с ведущим нулём, если нужно
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Получаем месяц (прибавляем 1, потому что месяцы начинаются с 0)
     const year = date.getFullYear(); // Получаем год
-  
     return `${day}.${month}.${year}`;
 }
 
 function checkTaskStatus(deadline){ //Проверяем статус задачи по дедлайну
     const deadlineDate = new Date(deadline * 1000);
     const timeNow = new Date();
-    if (deadlineDate.toDateString() === timeNow.toDateString()){return 'green'};
-    if (deadlineDate.toDateString() > timeNow.toDateString()) {return 'yellow'};
-    if (deadlineDate.toDateString() < timeNow.toDateString()) {return 'red'};
+    deadlineDate.setHours(0,0,0,0);
+    timeNow.setHours(0,0,0,0);
+    const deadlineDateTimestamp = deadlineDate.getTime() / 1000;
+    const timeNowTimestamp =timeNow.getTime() / 1000;
+    if(deadlineDateTimestamp === timeNowTimestamp) {return 'green';}
+    else if (deadlineDateTimestamp > timeNowTimestamp) {return 'yellow';}
+    else { {return 'red';}}
+
 }
 
-for(let i = 0; i < 10; i++){
+for(let i = 0; i < 10; i++){ //Запрашиваем информацию по конкретной задаче по ID
     leadDataAll[i].addEventListener("click", () => {
         for(let j = 0; j < 10; j++){
             if (!leadDetailsAll[j].classList.contains("is-hidden")){
@@ -52,8 +53,7 @@ for(let i = 0; i < 10; i++){
         })
         .then(data => {
             console.log('Ответ от API:', data);
-            // const leadName = leadDetailsAll[i].querySelector('.lead-details-name');
-            const leadName = leadDetailsAll[i].querySelector(".lead-details-name")
+            const leadName = leadDetailsAll[i].querySelector(".lead-details-name") //Заполняем данными html элементы
             const leadID = leadDetailsAll[i].querySelector(".lead-details-id");
             const leadDate = leadDetailsAll[i].querySelector(".lead-details-date");
             const leadTaskStatus = leadDetailsAll[i].querySelector(".lead-details-status circle");
